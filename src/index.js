147e40d194c8fb4d870eb9f69f94a99b5bab4fc6
Kevin Crawford <kvcrawford@gmail.com>
2015-12-03 10:30:08 -0800

    feat(app): add redux and counter
    Counter is included as an example module to make sure we've got everything wired up correctly.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import configureStore from './store/configureStore'
+
+const store = configureStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ src/reducers/widget.js
@@ -0,0 +1,5 @@
+const DEFAULT_STATE = []
+
+export default function widget(state = DEFAULT_STATE, action) {
+  return state
+}
