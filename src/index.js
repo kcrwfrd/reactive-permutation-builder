08d899e8a86e1c5655b2382e8887417515e5fe5a
Kevin Crawford <kvcrawford@gmail.com>
2015-12-03 11:44:04 -0800

    refactor(app): move App component into containers dir

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 
-import App from './App';
+import App from './containers/App';
 import configureStore from './store/configureStore'
 
 const store = configureStore()
